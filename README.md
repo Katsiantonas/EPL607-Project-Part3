67d036c6d5bd99022f6022f848cf52bce41fc046
akatsiantonis <akatsiantonis@hfmarkets.com>
2025-05-29 21:09:36 +0300

    Init commit for the project part 3

---
+++ README.md
@@ -0,0 +1,110 @@
+# EPL607 - Project Part 3
+
+
+## Introduction
+In this Project, we create a python program which receives the 3D coordinates of one triangle and 
+generates a series of PNG images that can later compose an MP4 animated video. This video displays
+a red triangle which is shaded with local illumination, rotating around itself.
+
+
+## Members
+- Antonis Katsiantonis
+- Nikolas Vattis
+
+
+## Programming Language and Libraries
+For the implementation of the second part of this project we decided to use the same programming 
+language (python) and library (pillow). Additionally, we will use the library numpy.
+
+
+## Entities
+
+For this part of the project we will need to operate on different entities, such as the Triangle, the
+Light source, etc. For this reason, we decided to create classes for each of these entities. In this 
+section we will discuss the attributes and behavior of each of these entities.
+
+
+### Vector
+
+For this part of the project, we needed to create a Vector entity, in order to be able to hold 3-dimensional
+vectors, which will then be used to compose more complex entities. Initially, we want a vector to store a 
+value for each of the x, y and z coordinates. Then, we want to implement all the operations that we want to
+use between two vectors, such as addition, multiplication, dot product, normalization, etc. This entity will 
+also be used to represent points in our coordinates, and will also represent colors where the x, y and z values 
+will represent the red, green and blue colors respectively.
+
+
+### Light
+
+Now that we have the Vector entity, we can also implement our Light entity, which will be defined by a position
+in our coordinates, and by the color that the light emits. Both of these attributes can be represented by our 
+Vector entity that we introduced in the previous section.
+
+
+### Material
+
+Material is another entity which can be implemented using Vectors. In order to represent the Material entity we
+will need to define it's diffuse and specular reflection, and also it's shininess. The diffuse and specular 
+reflections can be defined by vectors, having a value for each of the RGB colors. The shininess is represented 
+with a decimal number.
+
+
+### Triangle
+
+Now that we have the Vector and Material entities implemented, we want to create our Triangle entity, which 
+mainly holds two types of information: The 3D points for each triangle vertices, and the Material of the triangle.
+The positions of the vertices and the material will be represented by the Vector and Material entities respectively.
+
+
+## Transformations
+
+One of the additions that we chose to implement for this project is the Transformation, where our Triangle is being 
+transformed using the transformations that we learned in the class: Translation, Rotation, Scaling. We define 
+the transformation matrices in our project and use the python's matrix multiplication operation to multiply 
+our base triangle to retrieve the transformed triangle.
+
+
+## Projection
+In order to transform our shapes from 3D to 2D, we need to apply Perspective Projection into our 
+shapes. This operation is discussed in more detail in the second part of this project.
+
+
+## Rasterization
+Now that we have the 2D coordinates of our shapes, we now want to color the inside of our shapes.
+In order to do this, we need to take 2 things into consideration for each Pixel:
+
+* Is the pixel inside the shape? (We use edge function to determine this)
+* Is the pixel covered by another shape? (We use z-buffer to determine this)
+
+For each point of the triangle, if we find
+that the point is inside the triangle (w0 >= 0 and w1 >= 0 and w2 >= 0), then the only condition
+left to check is for other shapes that might be covering the pixel that we want to draw. The way
+we check this is by holding a z-buffer which holds the closest object for each pixel. So for our
+pixel, if the z value of our pixel is closer to the z value of the z-buffer (iterations of 
+previous shapes), then we should draw the pixel.
+
+
+## Shading
+
+Shading is the second addition that we chose to implement in the final part of this project, specifically
+Local Illumination. Initially we start by calculating the 3D position of our pixel, in order to apply the 
+Local Illumination formula on it. Then, we calculate the normal vector (perpendicular to the triangle's surface)
+which will be used to calculate both the Diffuse and Specular illumination. For the specular illumination, we 
+will also need to calculate the light and halfway vectors. After we calculate these vectors, we already have 
+the light's illumination, and also the diffuse and specular reflectance of our triangle, so now we can run
+the Local Illumination formula which will provide the color of our pixel.
+
+
+## Bounding Box
+The third addition we implemented is the bounding box, which reduces the time required to render the triangle.
+Initially, we find the Bounding Box (minX, maxX, minY, maxY) of our triangle in order to minimize
+the area that we will scan. Now that we have our Bounding Box, we will loop every pixel inside
+it, and for each pixel we will run the edge function for each line of the triangle.
+
+
+## Image Result
+![Image Result](rasterized_shapes.png "Image Result")
+...
+
+## Video
+...
---
+++ rasterizer-part-3.py
@@ -0,0 +1,206 @@
+from PIL import Image
+import math
+import numpy as np
+
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def dot(self, other):
+        return self.x*other.x + self.y*other.y + self.z*other.z
+
+    def length(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def normalize(self):
+        l = self.length()
+        if l == 0: return Vector(0, 0, 0)
+        return self * (1.0 / l)
+
+    def __repr__(self):
+        return f"Vector3({self.x}, {self.y}, {self.z})"
+
+class Material:
+    def __init__(self, diffuse, specular, shininess):
+        self.diffuse = diffuse
+        self.specular = specular
+        self.shininess = shininess
+
+
+class Light:
+    def __init__(self, position, color):
+        self.position = position
+        self.color = color
+
+
+class Triangle:
+    def __init__(self, v0, v1, v2, n0, n1, n2, material):
+        self.v0, self.v1, self.v2 = v0, v1, v2
+        self.n0, self.n1, self.n2 = n0, n1, n2
+        self.material = material
+
+
+def barycentric(p, a, b, c):
+    denom = (b.y - c.y)*(a.x - c.x) + (c.x - b.x)*(a.y - c.y)
+    if denom == 0:
+        return -1, -1, -1
+    w0 = ((b.y - c.y)*(p.x - c.x) + (c.x - b.x)*(p.y - c.y)) / denom
+    w1 = ((c.y - a.y)*(p.x - c.x) + (a.x - c.x)*(p.y - c.y)) / denom
+    w2 = 1 - w0 - w1
+    return w0, w1, w2
+
+
+class Renderer:
+    def __init__(self, width, height, camera_pos, light):
+        self.width = width
+        self.height = height
+        self.camera_pos = camera_pos
+        self.light = light
+        self.image = Image.new("RGB", (width, height))
+        self.pixels = self.image.load()
+        self.zbuffer = [[float('inf')] * width for _ in range(height)]
+
+    def project(self, v):
+        x = int((v.x + 1) * self.width / 2)
+        y = int((1 - v.y) * self.height / 2)
+        return Vector(x, y, v.z)
+
+    def render_triangle(self, tri):
+        p0 = self.project(tri.v0)
+        p1 = self.project(tri.v1)
+        p2 = self.project(tri.v2)
+
+        min_x = max(min(p0.x, p1.x, p2.x), 0)
+        max_x = min(max(p0.x, p1.x, p2.x), self.width - 1)
+        min_y = max(min(p0.y, p1.y, p2.y), 0)
+        max_y = min(max(p0.y, p1.y, p2.y), self.height - 1)
+
+        for y in range(min_y, max_y+1):
+            for x in range(min_x, max_x+1):
+                w0, w1, w2 = barycentric(Vector(x, y, 0), p0, p1, p2)
+                if w0 < 0 or w1 < 0 or w2 < 0:
+                    continue
+
+                z = tri.v0.z * w0 + tri.v1.z * w1 + tri.v2.z * w2
+                if z > self.zbuffer[y][x]:
+                    continue
+
+                self.zbuffer[y][x] = z
+
+                pos = tri.v0 * w0 + tri.v1 * w1 + tri.v2 * w2
+                normal = (tri.n0 * w0 + tri.n1 * w1 + tri.n2 * w2).normalize()
+
+                color = self.shade_pixel(pos, normal, tri.material)
+                self.pixels[x, y] = (
+                    int(color.x * 255),
+                    int(color.y * 255),
+                    int(color.z * 255)
+                )
+
+    def shade_pixel(self, pos, normal, material):
+        to_light = (self.light.position - pos).normalize()
+        to_view = (self.camera_pos - pos).normalize()
+
+        diff = max(normal.dot(to_light), 0)
+        diffuse = material.diffuse * diff
+
+        halfway = (to_light + to_view).normalize()
+        spec_angle = max(normal.dot(halfway), 0)
+        spec = (spec_angle ** material.shininess)
+        specular = material.specular * spec
+
+        color = Vector(
+            self.light.color.x * (diffuse.x + specular.x),
+            self.light.color.y * (diffuse.y + specular.y),
+            self.light.color.z * (diffuse.z + specular.z),
+        )
+
+        return Vector(
+            min(max(color.x, 0), 1),
+            min(max(color.y, 0), 1),
+            min(max(color.z, 0), 1),
+        )
+
+def vec3_to_np(v):
+    return np.array([v.x, v.y, v.z, 1.0])
+
+def np_to_vec3(arr):
+    return Vector(arr[0], arr[1], arr[2])
+
+def translate(tx, ty, tz):
+    return np.array([
+        [1, 0, 0, tx],
+        [0, 1, 0, ty],
+        [0, 0, 1, tz],
+        [0, 0, 0, 1]
+    ])
+
+def scale(sx, sy, sz):
+    return np.array([
+        [sx, 0,  0,  0],
+        [0, sy,  0,  0],
+        [0,  0, sz,  0],
+        [0,  0,  0,  1]
+    ])
+
+def rotate_y(theta):
+    c = math.cos(theta)
+    s = math.sin(theta)
+    return np.array([
+        [ c, 0, s, 0],
+        [ 0, 1, 0, 0],
+        [-s, 0, c, 0],
+        [ 0, 0, 0, 1]
+    ])
+
+def apply_transform(vec3, matrix):
+    transformed = matrix @ vec3_to_np(vec3)
+    return np_to_vec3(transformed)
+
+
+if __name__ == "__main__":
+    width, height = 400, 400
+    camera_pos = Vector(0, 0, 3)
+    light = Light(position=Vector(1, 1, 3), color=Vector(1, 1, 1))
+
+    red_material = Material(diffuse=Vector(1, 0, 0), specular=Vector(1, 1, 1), shininess=32)
+
+    base_tri = Triangle(
+        Vector(-0.5, -0.5, 0),
+        Vector(0.5, -0.5, 0),
+        Vector(0, 0.5, 0),
+        Vector(0, 0, 1),
+        Vector(0, 0, 1),
+        Vector(0, 0, 1),
+        red_material
+    )
+
+    for frame in range(60):
+        angle = frame * (2 * math.pi / 60)
+        transform = translate(0, 0, 0) @ rotate_y(angle) @ scale(1, 1, 1)
+
+        v0 = apply_transform(base_tri.v0, transform)
+        v1 = apply_transform(base_tri.v1, transform)
+        v2 = apply_transform(base_tri.v2, transform)
+        n0 = apply_transform(base_tri.n0, transform)
+        n1 = apply_transform(base_tri.n1, transform)
+        n2 = apply_transform(base_tri.n2, transform)
+
+        tri = Triangle(v0, v1, v2, n0.normalize(), n1.normalize(), n2.normalize(), red_material)
+
+        renderer = Renderer(width, height, camera_pos, light)
+        renderer.render_triangle(tri)
+
+        filename = f"frame_{frame:03d}.png"
+        renderer.image.save(filename)
+        print(f"Saved {filename}")
